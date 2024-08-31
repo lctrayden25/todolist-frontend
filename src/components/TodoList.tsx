cf38ee99bc589f88c6290533d2d210346a1e10c5
Rayden Li <79618253+lctrayden25@users.noreply.github.com>
2024-09-01 00:51:12 +0800

    update todo list item

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,23 +1,47 @@
-import { Button, Flex, Form, Input } from "antd";
-import { useCallback } from "react";
-// import { API_URL } from "../helpers/constant";
+import {
+	Button,
+	Card,
+	Flex,
+	Form,
+	Input,
+	List,
+	Typography,
+	Divider,
+} from "antd";
+import { useMemo } from "react";
+import {
+	CheckCircleOutlined,
+	DeleteOutlined,
+	ExclamationCircleOutlined,
+} from "@ant-design/icons";
+import { useTodoAction } from "../helpers/useTodoAction";
 
 type TodoType = {
 	todo: string;
 };
 
-const TodoList = () => {
-	// const [data, setData] = useState<undefined>();
-	// const [form] = Form.useForm();
+export type TodoData = {
+	id: string | number;
+	name: string;
+	status: string;
+};
 
-	const onFinish = useCallback(async (values: TodoType) => {
-		console.log(values);
-	}, []);
+const TodoList = () => {
+	const { createTodo, updateTodo, deleteTodo, todoItems, isLoading } =
+		useTodoAction();
+
+	const incompletedTodo = useMemo(() => {
+		return todoItems?.filter((todo) => todo.status !== "complete");
+	}, [todoItems]);
+
+	const completedTodo = useMemo(() => {
+		return todoItems?.filter((todo) => todo.status === "complete");
+	}, [todoItems]);
 
 	return (
 		<Form
 			// name="todoForm"
-			onFinish={onFinish}
+			onFinish={createTodo}
 			initialValues={{ todo: "" }}
 			autoComplete="off"
 			style={{ width: "100%" }}
@@ -26,7 +50,7 @@ const TodoList = () => {
 				<Form.Item<TodoType> name="todo" style={{ width: "100%" }}>
 					<Input
 						type="text"
-						placeholder="What are your targets to be done today ?"
+						placeholder={`What's your todo list today ?`}
 						size="large"
 					/>
 				</Form.Item>
@@ -36,6 +60,67 @@ const TodoList = () => {
 					</Button>
 				</Form.Item>
 			</Flex>
+			<Card title="Todo" loading={isLoading}>
+				<List
+					itemLayout="vertical"
+					dataSource={incompletedTodo}
+					renderItem={(item) => (
+						<List.Item
+							style={{
+								width: "100%",
+								display: "flex",
+								justifyContent: "space-between",
+							}}
+						>
+							<Flex align="center" gap="middle">
+								<ExclamationCircleOutlined style={{ color: "orange" }} />
+								<Typography.Text>{item?.name}</Typography.Text>
+							</Flex>
+							<Flex gap={5}>
+								<Button
+									type="primary"
+									onClick={() => updateTodo(item?.id as number)}
+									loading={isLoading}
+								>
+									Complete
+								</Button>
+								<Button
+									style={{ border: "none" }}
+									onClick={() => deleteTodo(item?.id as number)}
+									loading={isLoading}
+								>
+									<DeleteOutlined style={{ color: "red" }} />
+								</Button>
+							</Flex>
+						</List.Item>
+					)}
+				/>
+			</Card>
+			<Divider />
+			<Card title="Done" loading={isLoading}>
+				<List
+					itemLayout="vertical"
+					dataSource={completedTodo}
+					renderItem={(item) => (
+						<List.Item
+							style={{
+								width: "100%",
+								display: "flex",
+								justifyContent: "space-between",
+							}}
+						>
+							<Flex align="center" gap="middle">
+								<CheckCircleOutlined style={{ color: "green" }} />
+								<Typography.Text
+									style={{ color: "gray", textDecoration: "line-through" }}
+								>
+									{item?.name}
+								</Typography.Text>
+							</Flex>
+						</List.Item>
+					)}
+				/>
+			</Card>
 		</Form>
 	);
 };
